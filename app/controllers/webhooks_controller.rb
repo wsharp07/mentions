6a2e3c833e648a731faa103b4ac760de05e77985
ppworks <koshikawa@ppworks.jp>
2016-03-03 21:10:15 +0900

    Avoid "Can't verify CSRF token"

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,4 +1,6 @@
 class WebhooksController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
   def create
     @webhook = Webhook.find_by!(token: params[:token])
     @webhook.run(payload: params[:body])
