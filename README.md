9923acbc09213399f26ff025b229a406f85a447c
ppworks <koshikawa@ppworks.jp>
2016-03-10 16:37:55 +0900

    Use Incoming Webhooks in slack notification

--- README.md
+++ README.md
@@ -8,5 +8,5 @@ Webhook.create(from: 'github', to: 'slack', token: '7cae6a8984619abbdd7d32243b24
 
 ```
 export MENTIONS_MAPPING_FILE_PATH=https://***REMOVED***/mention_mappings.yml
-SLACK_WEBHOOK_URL=https://xxxx.slack.com/services/hooks/slackbot?token=yyyyyyyyyyyyyy
+export SLACK_WEBHOOK_URL=https://hook.slack.com/services/yyyyyyyyyyyyyy # Please use Incoming WebHooks
 ```
--- app/models/webhooks/to/slack.rb
+++ app/models/webhooks/to/slack.rb
@@ -7,17 +7,17 @@ class Webhooks::To::Slack
     @additional_message = additional_message
   end
 
-  def webhook_url(mention)
-    "#{ENV['SLACK_WEBHOOK_URL']}&channel=@#{mention}"
+  def webhook_url
+    ENV['SLACK_WEBHOOK_URL']
   end
 
   def run
     @mentions.each do |mention|
-      post(webhook_url(mention), "@#{mention} #{@url} #{@additional_message}")
+      post(webhook_url, "@#{mention}", "@#{mention} #{@url} #{@additional_message}")
     end
   end
 
-  def post(webhook, payload)
-    Net::HTTP.post_form(URI.parse(webhook), { payload: payload })
+  def post(webhook, channel, text)
+    Net::HTTP.post_form(URI.parse(webhook), { payload: {text: text, link_names: 1, channel: channel}.to_json })
   end
 end
