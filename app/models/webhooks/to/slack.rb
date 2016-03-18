baa9398dd080e222a95e9fcf5c22e786b3f3871d
Naoto Koshikawa <koshikawa@ppworks.jp>
2016-03-18 18:10:21 +0900

    Change channel for all mention

--- app/models/webhooks/to/slack.rb
+++ app/models/webhooks/to/slack.rb
@@ -1,23 +1,12 @@
 class Webhooks::To::Slack
-  attr_reader :from_instance
-
-  def initialize(mentions:, url:, additional_message:)
-    @mentions = mentions
-    @url = url
-    @additional_message = additional_message
+  def initialize(mention:, url:, additional_message:)
+    @mention = "@#{mention}"
+    @channel = @mention == '@all' ? '#general' : @mention
+    @text = "#{@mention} #{url} #{additional_message}"
+    @webhook_uri = URI.parse(ENV.fetch('SLACK_WEBHOOK_URL'))
   end
 
-  def webhook_url
-    ENV['SLACK_WEBHOOK_URL']
-  end
-
-  def run
-    @mentions.each do |mention|
-      post(webhook_url, "@#{mention}", "@#{mention} #{@url} #{@additional_message}")
-    end
-  end
-
-  def post(webhook, channel, text)
-    Net::HTTP.post_form(URI.parse(webhook), { payload: {text: text, link_names: 1, channel: channel}.to_json })
+  def post
+    Net::HTTP.post_form(@webhook_uri, { payload: {text: @text, link_names: 1, channel: @channel}.to_json })
   end
 end
