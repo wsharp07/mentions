67ee64789abac75a59bc26e7febd6aae8c2beb13
Naoto Koshikawa <koshikawa@ppworks.jp>
2016-03-18 18:45:49 +0900

    Set SLACK_WEBHOOK_URL env

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,6 @@
 ENV['RAILS_ENV'] ||= 'test'
 ENV['MENTIONS_MAPPING_FILE_PATH'] = "#{Rails.root}/test/files/mention_mappings.yml"
+ENV['SLACK_WEBHOOK_URL'] = 'http://example.com/slack'
 
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
