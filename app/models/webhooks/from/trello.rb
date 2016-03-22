d9a61696a64f9e3544561bef38cd71a312518d33
Naoto Koshikawa <koshikawa@ppworks.jp>
2016-03-22 21:35:52 +0900

    parse payload from trello

--- app/models/webhooks/from/trello.rb
+++ app/models/webhooks/from/trello.rb
@@ -2,10 +2,15 @@ class Webhooks::From::Trello < Webhooks::From::Base
   PATTERNS = %w()
 
   def comment
-    ''
+    @payload.dig('action', 'data', 'text')
   end
 
   def url
-    ''
+    "https://trello.com/c/#{@payload.dig('action', 'data', 'card', 'shortLink')}"
+  end
+
+  def mentions
+    return [] unless @payload.dig('action', 'type') =~ /\AcommentCard\z/
+    super
   end
 end
