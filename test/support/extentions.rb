3474427afacd0562d23e8a46afaee079ca642851
ppworks <koshikawa@ppworks.jp>
2016-12-11 00:01:29 +0900

    Refactor test helper

---
+++ test/support/extentions.rb
@@ -0,0 +1,3 @@
+class ActiveSupport::TestCase
+  fixtures :all
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,9 @@
-ENV['RAILS_ENV'] ||= 'test'
-ENV['MENTIONS_MAPPING_FILE_PATH'] = "#{Rails.root}/test/files/mention_mappings.yml"
-ENV['SLACK_WEBHOOK_URL'] = 'http://example.com/slack'
-
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'mocha/mini_test'
 
-class ActiveSupport::TestCase
-  fixtures :all
-end
+ENV['RAILS_ENV'] ||= 'test'
+ENV['MENTIONS_MAPPING_FILE_PATH'] = "#{Rails.root}/test/files/mention_mappings.yml"
+ENV['SLACK_WEBHOOK_URL'] = 'http://example.com/slack'
+
+Dir[Rails.root.join('test/support/**/*.rb')].each { |f| require f }
